a5b9d2834f34d0299229d02655f91be4b152ade9
lotterfriends <lotterfriends@gmail.com>
2013-01-30 23:03:29 +0100

    String Funktionen ausgelagert, Internationalisierung verwendet

--- app.js
+++ app.js
@@ -9,7 +9,10 @@ var express = require('express')
   , routes = require('./routes')
   , socket = require('socket.io')
   , bbcode = require('bbcode')
-  , smileyParser = require('./smileyParser.js');
+  , smileyParser = require('./lib/smileyParser.js')
+  , stringUtil = require('./lib/stringUtil.js')
+  , intNat = require('./lib/intNat.js');
+
 
 // Configuration
 
@@ -18,18 +21,23 @@ app.configure(function(){
 	app.set('views', __dirname + '/views');
 	app.set('view engine', 'jade');
 	app.set('view options', { layout: false });
+	app.set('env', 'development'); // production - development
+	app.set('server_username', 'SERVER');
 	app.use(express.bodyParser());
 	app.use(express.methodOverride());
 	app.use(app.router);
 	app.use(express.static(__dirname + '/public'));
+	intNat.load('de');
 });
 
 app.configure('development', function(){
 	app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+	app.set('port', 1338);
 });
 
 app.configure('production', function(){
 	app.use(express.errorHandler());
+	app.set('port', 1337);
 });
 
 // Routes
@@ -39,49 +47,45 @@ var usernames = {};
 var messages = [];
 
 io.sockets.on('connection', function (socket) {
-	
 	socket.on('adduser', function(username){
 		if (!username || username.trim().length < 1 ) {
-			return socket.emit('fehler', 'Kein Benutzername eingegeben');
+			return socket.emit('error', intNat.T("error.NoUsername"));
 		}
-		if (usernames[username] || username == "SERVER"){
-			return socket.emit('fehler', 'Benutzername bereits vergeben');
+		if (usernames[username] || username == app.get('server_username')){
+			return socket.emit('error', intNat.T("error.UsernameChoosen"));
 		}
 		if (username.length > 20) {
-			return socket.emit('fehler', 'Zu langer Benutzername');
+			return socket.emit('error', intNat.T("error.LongUsername"));
 		}
-		username = escapeHTML(username).trim();
+		username = stringUtil.escapeHTML(username).trim();
 		socket.username = username;
 		usernames[username] = username;
 		socket.emit('login', username);
-                for (var i in messages) {
+		for (var i in messages) {
 			socket.emit('updatechat', messages[i].username, messages[i].message,messages[i].time);
-                }
-		socket.emit('updatechat', 'SERVER', 'du bist verbunden');
-		socket.broadcast.emit('updatechat', 'SERVER', username + ' hat sich verbunden');
+		}
+		socket.emit('updatechat', app.get('server_username'), intNat.T("youConnected"));
+		socket.broadcast.emit('updatechat', app.get('server_username'), username + ' ' + intNat.T("xConnected"));
 		io.sockets.emit('updateusers', usernames);
 	});
 
 	socket.on('sendchat', function (data)  {
 		if (!socket.username) {
-			return socket.emit('fehler', 'Nicht eingeloggt!');
-		} 
-		if (!data) {
-			return socket.emit('fehler', 'Kein Text eingegeben.');
-        } 
-        if (data.length > 300) {
-			return socket.emit('fehler','Zu langer Text');
+			return socket.emit('error', intNat.T("error.notLoggedIn"));
 		}
-		data = escapeHTML(data).trim();
+		if (!data) {
+			return socket.emit('error', intNat.T("error.noMessage"));
+        }
+        if (data.length > 300) {
+			return socket.emit('error',intNat.T("error.longMessage"));
+		}
+		data = stringUtil.escapeHTML(data).trim();
 		bbcode.parse(data, function(content) {
-			content = smileyParser.parse(content);	
-			content = convertLinks(content);
+			content = smileyParser.parse(content);
+			content = stringUtil.convertLinks(content);
 			var time = new Date();
-				
 			messages.push({ "username" : socket.username, "time" : time, "message" : content });
-			if (messages.length > 30) {
-				messages.shift();
-			}
+			if (messages.length > 30) messages.shift();
 			io.sockets.emit('updatechat', socket.username, content, time);
 		});
 	});
@@ -89,35 +93,13 @@ io.sockets.on('connection', function (socket) {
 	socket.on('disconnect', function(){
 		delete usernames[socket.username];
 		io.sockets.emit('updateusers', usernames);
-		socket.broadcast.emit('updatechat', 'SERVER', socket.username + ' wurde ausgeloggt');
+		socket.broadcast.emit('updatechat', app.get('server_username'), socket.username + ' ' + intNat.T("xLogout"));
 	});
 
 });
 
-function escapeHTML(input) {
-	if (!input || input.trim() == "") return "";
-	return input
-	.replace(/&/g, '&amp;')
-        .replace(/</g, '&lt;')
-        .replace(/>/g, '&gt;');
-}
-
-function convertLinks(text){      
-		var sucher = new RegExp("(^|[ \t\r\n])((ftp|http|https|gopher|mailto|news|nntp|telnet|wais|file|prospero|aim|webcal):(([A-Za-z0-9$_.+!*(),;/?:@&~=-])|%[A-Fa-f0-9]{2}){2,}(#([a-zA-Z0-9][a-zA-Z0-9$_.+!*(),;/?:@&~=%-]*))?([A-Za-z0-9$_+!*();/?:~-]))","g");
-		var ergebnis = text.match(sucher);
-		if (ergebnis && ergebnis.length) {
-			for (var i in ergebnis)	{		
-				var regexp = new RegExp(ergebnis[i], "g");					
-				text = text.replace(regexp, " <a href='"+ergebnis[i].trim()+"' target='_blank'>" + ergebnis[i].trim() + "</a>");
-			}
-		}		
-		return text;		
-}
-
-
-
-
-server.listen(1337, function(){
-	console.log("Der Chat wurde auf Port 1337 gestartet");
+server.listen(app.get('port'), function(){
+	console.log("Enviroment: ", app.get('env'));
+	console.log("Port: ", app.get('port'));
 });
 

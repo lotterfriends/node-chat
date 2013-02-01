fbca7478eb97a35e4661208f4c8454a6353d9a19
lotterfriends <lotterfriends@gmail.com>
2013-02-01 23:01:45 +0100

    Umformatiert und besser Strukturiert

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 /**
- * Module dependencies.
+ * Module dependencies & Global Variables.
  */
-
 var express = require('express')
   , app = express()
   , server = require('http').createServer(app)
@@ -11,11 +10,12 @@ var express = require('express')
   , bbcode = require('bbcode')
   , smileyParser = require('./lib/smileyParser.js')
   , stringUtil = require('./lib/stringUtil.js')
-  , intNat = require('./lib/intNat.js');
+  , intNat = require('./lib/intNat.js')
+  , usernames = {}
+  , messages = [];
 
 
 // Configuration
-
 app.configure(function(){
 	app.set('title', 'Chat');
 	app.set('views', __dirname + '/views');
@@ -23,6 +23,7 @@ app.configure(function(){
 	app.set('view options', { layout: false });
 	app.set('env', 'development'); // production - development
 	app.set('server_username', 'SERVER');
+	app.set('saved_messages_count', 30);
 	app.use(express.bodyParser());
 	app.use(express.methodOverride());
 	app.use(app.router);
@@ -33,78 +34,113 @@ app.configure(function(){
 app.configure('development', function(){
 	app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 	app.set('port', 1338);
+	
 });
 
 app.configure('production', function(){
 	app.use(express.errorHandler());
 	app.set('port', 1337);
+	io.set('log level', 1);
 });
 
 // Routes
 app.get('/', routes.index);
 
-var usernames = {};
-var messages = [];
-
+// Server Events
 io.sockets.on('connection', function (socket) {
-	socket.on('adduser', function(username){
-		if (!username || username.trim().length < 1 ) {
-			return socket.emit('error', intNat.T("error.NoUsername"));
-		}
-		if (usernames[username] || username == app.get('server_username')){
-			return socket.emit('error', intNat.T("error.UsernameChoosen"));
-		}
-		if (username.length > 20) {
-			return socket.emit('error', intNat.T("error.LongUsername"));
-		}
-		if (username.length < 4) {
-			return socket.emit('error', intNat.T("error.ShortUsername"));
-		}
-		username = stringUtil.escapeHTML(username).trim();
-		socket.username = username;
-		usernames[username] = username;
-		socket.emit('login', username);
-		for (var i in messages) {
-			socket.emit('updatechat', messages[i].username, messages[i].message,messages[i].time);
-		}
-		socket.emit('updatechat', app.get('server_username'), intNat.T("youConnected"));
-		socket.broadcast.emit('updatechat', app.get('server_username'), username + ' ' + intNat.T("xConnected"));
-		io.sockets.emit('updateusers', usernames);
-	});
-
-	socket.on('sendchat', function (data)  {
-		if (!socket.username) {
-			return socket.emit('error', intNat.T("error.notLoggedIn"));
-		}
-		if (!data) {
-			return socket.emit('error', intNat.T("error.noMessage"));
-        }
-        if (data.length > 300) {
-			return socket.emit('error',intNat.T("error.longMessage"));
-		}
-		data = stringUtil.escapeHTML(data).trim();
-		bbcode.parse(data, function(content) {
-			content = smileyParser.parse(content);
-			content = stringUtil.convertLinks(content);
-			var time = new Date();
-			messages.push({ "username" : socket.username, "time" : time, "message" : content });
-			if (messages.length > 30) messages.shift();
-			io.sockets.emit('updatechat', socket.username, content, time);
-		});
-	});
-
-	socket.on('disconnect', function(){
-		delete usernames[socket.username];
-		if (socket.username) {
-			io.sockets.emit('updateusers', usernames);
-		}
-		socket.broadcast.emit('updatechat', app.get('server_username'), socket.username + ' ' + intNat.T("xLogout"));
-	});
-
+	socket.on('login', login);
+	socket.on('sendchat', sendchat);
+	socket.on('disconnect', disconnect);
 });
 
+// Start Server
 server.listen(app.get('port'), function(){
 	console.log("Enviroment: ", app.get('env'));
 	console.log("Port: ", app.get('port'));
 });
 
+function login(username) {
+	var socket = this;
+	validateUsername(username, function(error) {
+		if (error) return socket.emit('error', error);
+		username = stringUtil.escapeHTML(username).trim();
+		socket.username = username;
+		usernames[username] = username;
+		socket.emit('login', username);
+		loadMessages();
+		socket.emit('updatechat', app.get('server_username'), intNat.T("user.youConnected"));
+		socket.broadcast.emit('updatechat', app.get('server_username'), intNat.T("user.hasConnected", username));
+		io.sockets.emit('updateusers', usernames);
+	});
+}
+
+function sendchat(message) {
+	var socket = this;
+	validateMessage(socket.username, message, function(error) {
+		if (error) return socket.emit('error', error);
+		message = stringUtil.escapeHTML(message).trim();
+		bbcode.parse(message, function(content) {
+			content = smileyParser.parse(content);
+			content = stringUtil.convertLinks(content);
+			var time = new Date();
+			messages.push({ "username" : socket.username, "time" : time, "message" : content });
+			if (messages.length > app.get('saved_messages_count')) messages.shift();
+			io.sockets.emit('updatechat', socket.username, content, time);
+		});
+	});
+}
+
+function disconnect() {
+	var socket = this;
+	if (socket.username) {
+		delete usernames[socket.username];
+		io.sockets.emit('updateusers', usernames);
+		socket.broadcast.emit('updatechat', app.get('server_username'), intNat.T("user.logedout",socket.username));	
+	}
+}
+
+function loadMessages() {
+	for (var i in messages) {
+		socket.emit('updatechat', messages[i].username, messages[i].message, messages[i].time);
+	}
+}
+
+function validateUsername(username, callback) {
+	var MAX_LENGTH = 20;
+	var MIN_LENGTH = 4;
+	if (!username || username.trim().length < 1) {
+		 callback(intNat.T("error.NoUsername"));
+		 return;
+	}
+	if (usernames[username] || username == app.get('server_username')) {
+		callback(intNat.T("error.UsernameChoosen"));
+		return;
+	}
+	if (username.length > MAX_LENGTH) {
+		callback(intNat.T("error.toLongUsername"));
+		return;
+	}
+	if (username.length < MIN_LENGTH) {
+		callback(intNat.T("error.toShortUsername"));
+		return;
+	}
+	callback();
+}
+
+function validateMessage(username, message, callback) {
+	var MAX_LENGTH = 300;
+	var MIN_LENGTH = 1;
+	if (!username) {
+		callback(intNat.T("error.notLoggedIn"));
+		return;
+	}
+	if (!message || message.trim().length < MIN_LENGTH) {
+		callback(intNat.T("error.noMessage"));
+		return;
+	}
+	if (message.length > MAX_LENGTH) {
+		callback(intNat.T("error.longMessage"));
+		return;
+	}
+	callback();
+}
--- public/js/chat.js
+++ public/js/chat.js
@@ -1,122 +1,93 @@
-var socket = io.connect('http://localhost');
-var state, visibilityChange; 
+// Global Vars
+var socket = io.connect('http://localhost')
+	, state
+	, visibilityChange
+	, fehler_id = 0
+	, pageReadyFunctions = []
+	, KEYCODE_ENTER = 13;
 
+// Client Events
+socket.on('connect', showDialog);
+socket.on('error', handleError);
+socket.on('login', login);
+socket.on('updatechat', updateChat);
+socket.on('updateusers', updateUser);
 
-socket.on('connect', function(){
-	$('#login').modal('show');
-});
-
-var fehler_id = 0;
-socket.on('error', function(message){
-	var $fehlerContainer = $('#login .modal-body:visible');
-	if (!$fehlerContainer.length) {
-		$fehlerContainer = $('.content.chat');
+function handleError(message) {
+	var DISPLAY_TIME = 6000;
+	// 6 Sec.
+	var $errorContainer = $('#login .modal-body:visible');
+	if (!$errorContainer.length) {
+		$errorContainer = $('.content.chat');
 	}
-	var $fehler = $('.alert', $fehlerContainer);
-	if ($fehler.length) {
+	var $error = $('.alert', $errorContainer);
+	if ($error.length) {
 		window.clearTimeout(fehler_id);
-		$fehler.html(message).show();
+		$error.html(message).show();
 	} else {
 		var $message = $('<div class="alert alert-error">' + message + '</div>');
-		$fehlerContainer.prepend($message);
+		$errorContainer.prepend($message);
 	}
-	$fehlerContainer.ready(function() {
-		$('input:first', $fehlerContainer).focus();
+	$errorContainer.ready(function() {
+		$('input:first', $errorContainer).focus();
 		fehler_id = window.setTimeout(function() {
-			$('.alert', $fehlerContainer).hide('slow');
-		},6000);
+			$('.alert', $errorContainer).hide('slow');
+		}, DISPLAY_TIME);
 	});
-	if(!$(window).data('user')) {
-		$('#login').modal('show');
+	if (!$(window).data('user')) {
+		showDialog();
 	}
-});
+}
 
-socket.on('login', function(username) {
+function showDialog() {
+	$('#login').modal('show');
+}
+
+function hideDialog() {
 	$('#login').modal('hide');
+}
+
+function focusSendField() {
+	$('#data').focus();
+}
+
+function login(username) {
+	hideDialog();
 	$(window).data('user', username);
-});
+}
 
-
-socket.on('updatechat', function (username, data, time) {
-	if (!time) time = new Date();
+function updateChat(username, message, time) {
+	if (!time)
+		time = new Date();
 	var zeit = Date.create(time).format('{HH}:{mm}:{ss}');
 	var $time = '<span class="time">(' + zeit + ') </span>';
-	var $user = '<span class="username">'+ username + ': </span>';
-	var $message = '<span class="message">' + data +'</span>';
+	var $user = '<span class="username">' + username + ': </span>';
+	var $message = '<span class="message">' + message + '</span>';
 	var $entry = '<p class="entry">' + $time + $user + $message + '</p>';
 	$('#messages-inner').append($entry);
 	$('#messages-inner').scrollTo($('#messages-inner .entry:last'));
-	$('#data').focus();
+	focusSendField();
 	if (document[state] == "hidden") {
+		$(document).data('title', document.title);
 		$.titleAlert("Neue Nachricht!");
 	}
-	
-});
+}
 
-socket.on('updateusers', function(data) {
+function updateUser(users) {
 	$('#users ul').empty();
 	$('#users ul').append('<li class="nav-header">Online</li>');
-	$.each(data, function(key, value) {
+	$.each(users, function(key, value) {
 		var activeCSSClass = '';
 		var user = '<a href="#">' + key + '</a>';
-		if ($(window).data('user') == key){
+		if ($(window).data('user') == key) {
 			activeCSSClass = ' class="active" ';
 			user = key;
 		}
-		$('#users ul').append('<li' + activeCSSClass +'>'+ user +'</li>');
+		$('#users ul').append('<li' + activeCSSClass + '>' + user + '</li>');
 	});
-});
+}
 
-$(function(){
-	$('#datasend').click( function() {
-		var message = $('#data').val();
-		$('#data').val('');
-		socket.emit('sendchat', message);
-	});
-
-	$('#data').keypress(function(e) {
-		if(e.which == 13) {
-			$(this).blur();
-			$('#datasend').click();
-		}
-		var akt_count = $(this).val().length;
-		$('.count').html(300 - akt_count);
-	});
-	
-	$('#data').on('keyup', function() {
-		var akt_count = $(this).val().length;
-		$('.count').html(300 - akt_count);
-	});
-
-	$('#messages-inner').dblclick(function() {
-		$('#messages-inner .entry').remove();
-	});
-	
-	$('#users #user-list').on('click', 'a', function(e) {
-		$('#data').val('@' + $(this).text() + ' ' + $('#data').val()).focus();
-		e.stopPropagation();
-		return false;
-	});
-
-	$('#login .btn.btn-primary').click(function() {
-		var username = $.trim($('#login input[name=nickname]').val());
-		socket.emit('adduser', username);
-	});
-
-    $('#login').on('shown', function () {
-		$('#login input[name=nickname]:first').focus().select();
-		$('.modal-backdrop.fade.in').unbind('click')
-    });
-
-	$('#login input[name=nickname]:first').on('keypress', function(e) {
-		if(e.which == 13) {
-			$(this).blur();
-			$('#login .btn.btn-primary').click();
-			e.stopPropagation();
-			return false;
-		}
-	});
-
+function initPuller() {
 	$('.puller').on('mousedown', function(e) {
 		var $puller = $(this);
 		var $messages = $puller.prevAll('#messages-inner');
@@ -138,25 +109,85 @@ $(function(){
 			$messages.height(height);
 		}
 	});
-	
-	if (typeof document.hidden !== "undefined") {
-  		visibilityChange = "visibilitychange";
-  		state = "visibilityState";
-	} else if (typeof document.mozHidden !== "undefined") {
-  		visibilityChange = "mozvisibilitychange";
-  		state = "mozVisibilityState";
-	} else if (typeof document.msHidden !== "undefined") {
-  		visibilityChange = "msvisibilitychange";
-  		state = "msVisibilityState";
-	} else if (typeof document.webkitHidden !== "undefined") {
-  		visibilityChange = "webkitvisibilitychange";
-		 state = "webkitVisibilityState";
+}
+
+pageReadyFunctions.push(initPuller);
+
+function initVisibilityStateListener() {
+	if ( typeof document.hidden !== "undefined") {
+		visibilityChange = "visibilitychange";
+		state = "visibilityState";
+	} else if ( typeof document.mozHidden !== "undefined") {
+		visibilityChange = "mozvisibilitychange";
+		state = "mozVisibilityState";
+	} else if ( typeof document.msHidden !== "undefined") {
+		visibilityChange = "msvisibilitychange";
+		state = "msVisibilityState";
+	} else if ( typeof document.webkitHidden !== "undefined") {
+		visibilityChange = "webkitvisibilitychange";
+		state = "webkitVisibilityState";
 	}
 
 	$(document).on(visibilityChange, function() {
 		if (document[state] == 'visible') {
-			document.title = "Chat";
+			document.title = $(document).data('title');
 		}
 	});
-	
+}
+
+pageReadyFunctions.push(initVisibilityStateListener);
+
+function initCountChars() {
+	$('#data').on('keyup', function() {
+		var akt_count = $(this).val().length;
+		$('.count').html(300 - akt_count);
+	});
+}
+
+pageReadyFunctions.push(initCountChars);
+
+$(function() {
+
+	for (var i in pageReadyFunctions) {
+		pageReadyFunctions[i]();
+	}
+
+	$('#datasend').click(function() {
+		var message = $('#data').val();
+		$('#data').val('');
+		socket.emit('sendchat', message);
+	});
+
+	$('#data').keypress(function(e) {
+		if (e.which == KEYCODE_ENTER) {
+			$(this).blur();
+			$('#datasend').click();
+		}
+	});
+
+	$('#users #user-list').on('click', 'a', function(e) {
+		$('#data').val('@' + $(this).text() + ' ' + $('#data').val()).focus();
+		e.stopPropagation();
+		return false;
+	});
+
+	$('#login .btn.btn-primary').click(function() {
+		var username = $.trim($('#login input[name=nickname]').val());
+		socket.emit('login', username);
+	});
+
+	$('#login').on('shown', function() {
+		$('#login input[name=nickname]:first').focus().select();
+		$('.modal-backdrop.fade.in').unbind('click')
+	});
+
+	$('#login input[name=nickname]:first').on('keypress', function(e) {
+		if (e.which == KEYCODE_ENTER) {
+			$(this).blur();
+			$('#login .btn.btn-primary').click();
+			e.stopPropagation();
+			return false;
+		}
+	});
+
 });
